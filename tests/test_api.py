708d3331a3dfd734c3056b05e9534c88ad8953c1
Michaël Arnauts <michael.arnauts@gmail.com>
2021-01-08 14:27:04 +0100

    Improve tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,6 +17,7 @@ from resources.lib.solocoo.exceptions import NotAvailableInOfferException
 _LOGGER = logging.getLogger(__name__)
 
 
+@unittest.skipUnless(kodiutils.get_setting('username') and kodiutils.get_setting('password'), 'Skipping since we have no credentials.')
 class TestChannel(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestChannel, self).__init__(*args, **kwargs)
@@ -26,16 +27,14 @@ class TestChannel(unittest.TestCase):
                              kodiutils.get_setting('tenant'),
                              kodiutils.get_tokens_path())
 
-    @unittest.skipUnless(kodiutils.get_setting('username') and kodiutils.get_setting('password'), 'Skipping since we have no credentials.')
     def test_get_channels(self):
         api = ChannelApi(self._auth)
 
         channels = api.get_channels()
-        print(channels)
         self.assertIsInstance(channels, list)
         self.assertIsInstance(channels[0], Channel)
-        self.assertIsInstance(channels[0].uid, str)
-        self.assertIsInstance(channels[0].station_id, str)
+        self.assertIsNotNone(channels[0].uid)
+        self.assertIsNotNone(channels[0].station_id)
 
     def test_get_channel(self):
         api = ChannelApi(self._auth)
@@ -43,17 +42,14 @@ class TestChannel(unittest.TestCase):
         channel_id = 'V6sXTJf1I6krfS3MRe0Dd5UGqFczlxHlZ86MLQ_R'  # VTM
 
         channel = api.get_asset(channel_id)
-        print(channel)
         self.assertIsInstance(channel, Channel)
 
-    @unittest.skipUnless(kodiutils.get_setting('username') and kodiutils.get_setting('password'), 'Skipping since we have no credentials.')
     def test_get_channel_stream(self):
         api = ChannelApi(self._auth)
 
         channel_id = 'V6sXTJf1I6krfS3MRe0Dd5UGqFczlxHlZ86MLQ_R'  # VTM
 
         stream_info = api.get_stream(channel_id)
-        print(stream_info)
         self.assertIsInstance(stream_info, StreamInfo)
 
     def test_get_channel_without_package(self):
@@ -73,14 +69,12 @@ class TestChannel(unittest.TestCase):
         channel_id = 'JIY-fyHDkM1Rk260f-WNXlVD8iYnlDtWOQ4ah0hb'  # één
 
         programs = api.get_replay(channel_id)
-        print(programs)
         self.assertIsInstance(programs, list)
         self.assertIsInstance(programs[0], Program)
 
         # Find a serie
         series_id = next((program.series_id for program in programs if program.series_id is not None))
         programs = api.get_series(series_id)
-        print(programs)
         self.assertIsInstance(programs, list)
         self.assertIsInstance(programs[0], Program)
 
--- tests/test_routing.py
+++ tests/test_routing.py
@@ -5,13 +5,19 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import logging
 import unittest
 
 from resources.lib import addon, kodiutils
 
 routing = addon.routing  # pylint: disable=invalid-name
 
+_LOGGER = logging.getLogger(__name__)
 
+EXAMPLE_CHANNEL = 'JIY-fyHDkM1Rk260f-WNXlVD8iYnlDtWOQ4ah0hb:1790975744'  # één
+
+
+@unittest.skipUnless(kodiutils.get_setting('username') and kodiutils.get_setting('password'), 'Skipping since we have no credentials.')
 class TestRouting(unittest.TestCase):
     """ Tests for Routing """
 
@@ -23,12 +29,22 @@ class TestRouting(unittest.TestCase):
         # warnings.simplefilter("ignore", ResourceWarning)
         pass
 
+    def test_index(self):
+        routing.run([routing.url_for(addon.index), '0', ''])
+
     def test_main_menu(self):
         routing.run([routing.url_for(addon.show_main_menu), '0', ''])
 
-    @unittest.skipUnless(kodiutils.get_setting('username') and kodiutils.get_setting('password'), 'Skipping since we have no credentials.')
     def test_channels_menu(self):
         routing.run([routing.url_for(addon.show_channels), '0', ''])
+        routing.run([routing.url_for(addon.show_channel, channel_id=EXAMPLE_CHANNEL), '0', ''])
+        routing.run([routing.url_for(addon.show_channel_guide, channel_id=EXAMPLE_CHANNEL), '0', ''])
+        routing.run([routing.url_for(addon.show_channel_guide_detail, channel_id=EXAMPLE_CHANNEL, date='today'), '0', ''])
+        routing.run([routing.url_for(addon.show_channel_replay, channel_id=EXAMPLE_CHANNEL), '0', ''])
+
+    def test_search_menu(self):
+        routing.run([routing.url_for(addon.show_search), '0', ''])
+        routing.run([routing.url_for(addon.show_search, query='vier'), '0', ''])
 
 
 if __name__ == '__main__':
